8d3909a5a3e95ea7bef908f3625adf57ac0b3b0f
zetsuen160 <43133711+zetsuen160@users.noreply.github.com>
2019-12-23 22:04:06 +0500

    движение N тел

---
+++ движение n тел/Assets/Scripts/fosrce.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fosrce : MonoBehaviour
+{
+
+    public Vector3 force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(force);
+    }
+
+}
