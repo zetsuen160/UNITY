33ce11d1d22a2553e85a07551631d3faa0225b42
zetsuen160 <43133711+zetsuen160@users.noreply.github.com>
2019-12-23 21:07:52 +0500

    движение тел

---
+++ движение тел/Assets/Scripts/begin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class begin : MonoBehaviour
+{
+
+
+    // Instantiates a prefab in a circle
+
+    public GameObject prefab;
+    public int numberOfObjects = 5;
+    public float radius = 5f;
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0 ) * radius;
+            Instantiate(prefab, pos, Quaternion.identity);
+        }
+    }
+}
